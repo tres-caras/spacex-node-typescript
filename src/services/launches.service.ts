c45e370a4fc7803f8666f76d1cd1a0b2e88dbd2f
tres-caras <severinidiegoignacio@gmail.com>
2022-08-15 10:57:49 -0300

    working offline

--- src/services/launches.service.ts
+++ src/services/launches.service.ts
@@ -2,44 +2,63 @@ import axios from "axios";
 import { Launch } from "../models/Launch.model";
 import { Payload } from "../models/Payload.model";
 import { Rocket } from "../models/Rocket.model";
-import { Paginated } from "../models/Paginated.model"; 
+import { Paginated } from "../models/Paginated.model";
 
-const getLaunchesOnly = async (url: string): Promise<Record<string, any>> => {
-  let returnValue: Object[] = [];
-  await axios
-    .get(url)
-    .then((response) => response.data)
-    .then((launches: Record<string, any>) => {
-      launches.forEach((launch: Record<string, any>) => {
-        const myLaunchObject = {
-          flight_number: launch.flight_number,
-          mission_name: launch.mission_name,
-          rocket: launch.rocket,
-          payloads: launch.rocket.second_stage.payloads,
-        };
-        returnValue.push(myLaunchObject);
-      });
-    });
+type PayloadResponse = {
+  payloads: Payload[];
+}
+
+type RocketResponse = {
+  rocket_id: string;
+  rocket_name: string;
+  description: string;
+  images: string[];
+  second_stage: PayloadResponse;
+}
+
+type LaunchesResponse= {
+  flight_number: number;
+  mission_name: string;
+  rocket: RocketResponse;
+  payloads: Payload[];
+}
+
+//caching (e.g: to a json) allows offline mode
+const getLaunchesOnly = async (url: string): Promise<LaunchesResponse[]> => {
+  let returnValue: LaunchesResponse[] = [];
+  const response = await axios.get(url);
+  const launches = response.data;
+
+  launches.forEach((launch: LaunchesResponse) => {
+    const myLaunchObject = {
+      flight_number: launch.flight_number,
+      mission_name: launch.mission_name,
+      rocket: launch.rocket,
+      payloads: launch.rocket.second_stage.payloads,
+    };
+    returnValue.push(myLaunchObject);
+  });
+  
   return returnValue;
 };
 
-const getRocket = async (url: string): Promise<Record<string, any>> => {
+//caching (e.g: to a json) allows offline mode
+const getRockets = async (url: string): Promise<Record<string, any>> => {
   let returnValue: Object[] = [];
-  await axios
-    .get(url)
-    .then((response) => response.data)
-    .then((rockets: Record<string, any>) => {
-      rockets.forEach((rocket: Record<string, any>) => {
-        const myRocketObject = {
-          rocket_id: rocket.rocket_id,
-          rocket_name: rocket.rocket_name,
-          description: rocket.description,
-          images: rocket.flickr_images,
-          payloads: rocket.second_stage.payloads,
-        };
-        returnValue.push(myRocketObject);
-      });
-    });
+  const response = await axios.get(url);
+  const rockets = response.data;
+
+  rockets.forEach((rocket: Record<string, any>) => {
+    const myRocketObject = {
+      rocket_id: rocket.rocket_id,
+      rocket_name: rocket.rocket_name,
+      description: rocket.description,
+      images: rocket.flickr_images,
+      payloads: rocket.second_stage.payloads,
+    };
+    returnValue.push(myRocketObject);
+  });
+
   return returnValue;
 };
 
@@ -68,39 +87,40 @@ function getPayloadMerge(
 }
 
 async function getLaunches(): Promise<Launch[]> {
-  return Promise.all([
-    getRocket("https://api.spacexdata.com/v3/rockets"),
-    getLaunchesOnly("https://api.spacexdata.com/v3/launches"),
-  ]).then(([rockets, launches]) => {
-    const myLaunches = launches.map((launch: Record<string, any>) => {
-      let theRocket = rockets.find((rocket: Record<string, any>) => {
-        if (rocket.rocket_id === launch.rocket.rocket_id) {
-          return rocket;
-        }
-      });
-      return new Launch(
-        launch.flight_number,
-        launch.mission_name,
-        getRocketMerge(theRocket, launch.rocket),
-        getPayloadMerge(theRocket, launch.rocket)
-      );
+  const [rockets, launches] = await Promise.all([
+    getRockets(`https://api.spacexdata.com/v3/rockets`),
+    getLaunchesOnly(`https://api.spacexdata.com/v3/launches`),
+  ]);
+
+  const myLaunches = launches.map((launch: LaunchesResponse) => {
+    let theRocket = rockets.find((rocket: RocketResponse) => {
+      if (rocket.rocket_id === launch.rocket.rocket_id) {
+        return rocket;
+      }
     });
-    return myLaunches;
+    return new Launch(
+      launch.flight_number,
+      launch.mission_name,
+      getRocketMerge(theRocket, launch.rocket),
+      getPayloadMerge(theRocket, launch.rocket)
+    );
   });
+  return myLaunches;
+  
 }
 
-export default async function getPaginatedLaunches(limit: number,page: number): Promise<Paginated<Launch>> {
+export default async function getPaginatedLaunches(
+  limit: number,
+  page: number
+): Promise<Paginated<Launch>> {
   const launches = await getLaunches();
-    const paginatedLaunches = launches.slice(
-      (page - 1) * limit,
-      page * limit
-    );
+  const paginatedLaunches = launches.slice((page - 1) * limit, page * limit);
 
-    return {
-      data: paginatedLaunches,
-      total: launches.length,
-      per_page: limit,
-      page,
-      pages: Math.ceil(launches.length / limit),
-    };
+  return {
+    data: paginatedLaunches,
+    total: launches.length,
+    per_page: limit,
+    page,
+    pages: Math.ceil(launches.length / limit),
+  };
 }
